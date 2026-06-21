34050cc9257ce2986f855b4ec2566bea0717937c
jianglipi314 <1281582261@qq.com>
2026-06-21 22:41:27 +0800

    remove old Vercel API files

--- api/complete.js
+++
@@ -1,1 +0,0 @@
-module.exports = (req, res) => { res.setHeader("Access-Control-Allow-Origin","*"); if (req.method === "OPTIONS") { return res.status(200).end(); } return res.status(200).json({ success: true }); };
